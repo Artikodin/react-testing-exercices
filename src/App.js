c53346fb7a4926b30839275bad8d15a1eb00af49
Artium Wasse <artium.wasse@vimcar.com>
2022-02-08 16:47:31 +0100

    feat(MultiToggle): Add multi toggle component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Toggle from './Toggle'
+import MultiToggle from './MultiToggle'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Toggle />
       </div>
       <div className='container'>
-        <Toggle />
+        <MultiToggle />
       </div>
       <div className='container'>
         <Toggle />
